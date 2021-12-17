4bad4f3c4f4ae302738ecb276e82c29987a9288f
matangeorgi <87900560+matangeorgi@users.noreply.github.com>
2021-12-17 20:41:13 +0200

    Almost finished first styling preparations with the topbar at the homepage

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from "react";
+import Topbar from "./Topbar";
 import { ImHome,ImSearch,ImBell,ImUser } from "react-icons/im";
 import {Navbar} from "../styles/Navbar.styled";
 import {IconContext} from "react-icons";
@@ -7,13 +8,15 @@ import {IconContext} from "react-icons";
 const Home = () => {
     return (
         <div>
-            <IconContext.Provider value={{ color: '5450bd', size: '30px' }}>
-            <Navbar className ='navbar d-flex justify-content-center'>
-                <a href="#home"><ImHome/></a>
-                <a href="#news"><ImBell/></a>
-                <a href="#contact">Contact</a>
-            </Navbar>
-            </IconContext.Provider>
+            {/*<IconContext.Provider value={{ size: '30px' }}>*/}
+            {/*<Navbar className ='navbar d-flex justify-content-center'>*/}
+            {/*    <a href="#home"><ImHome/></a>*/}
+            {/*    <a href="#news"><ImBell/></a>*/}
+            {/*    <a href="#contact"><ImSearch/></a>*/}
+            {/*    <a href="#contact"><ImUser/></a>*/}
+            {/*</Navbar>*/}
+            {/*</IconContext.Provider>*/}
+            <Topbar></Topbar>
         </div>
     );
 }
--- frontend/src/components/styles/Navbar.styled.js
+++ frontend/src/components/styles/Navbar.styled.js
@@ -1,16 +1,69 @@
 import styled from "styled-components"
 
-export const Navbar = styled.div`
+export const TopBar = styled.div`
   top: 0;
   width: 100%;
   height: 60px;
-  overflow: hidden;
+  display: flex;
+  position: sticky;
+  align-items: center;
   background-color: white;
-  position: fixed; 
-  
   box-shadow: 0px 0px 5px #b9b9b9;
+  
+`
 
-  a{
-    padding: 20px;
+export const Logo = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+  cursor: pointer;
+`
+
+export const Input = styled.input`
+  width: 400px;
+  height: 30px;
+  background-color: white;
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+`
+
+export const TopBarLeft = styled.div`
+    flex: 3;
+`
+
+export const TopBarCenter = styled.div`
+    flex: 5;
+`
+export const TopBarRight = styled.div`
+  flex: 4;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .topbarIcons {
+    display: flex;
+  }
+  .topbarIconItem {
+    margin-right: 15px;
+    cursor: pointer;
+    position: relative;
+  }
+  .topbarIconBadge {
+    width: 15px;
+    height: 15px;
+    background-color: red;
+    border-radius: 50%;
+    color: white;
+    position: absolute;
+    top: -5px;
+    right: -5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
+  }
+  .topbarLink {
+    margin-right: 10px;
+    font-size: 14px;
+    cursor: pointer;
   }
 `
